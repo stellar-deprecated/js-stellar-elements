b3f80b051eabdb2fc8884174a5d08d93a5e504da
Morley Zhi <morley@stellar.org>
2019-04-22 16:31:17 -0400

    Input

---
+++ src/Input.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+  border: 1px solid blue;
+  border-radius: 2px;
+`;
+
+export default Input;
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -2,18 +2,12 @@ import React from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-import { linkTo } from "@storybook/addon-links";
 
 import Button from "Button";
+import Input from "Input";
 
-storiesOf("Button", module)
-  .add("with text", () => (
-    <Button onClick={action("clicked")}>Hello Button</Button>
+storiesOf("Form elements", module)
+  .add("Button", () => (
+    <Button onClick={action("clicked")}>Mark button as clicked</Button>
   ))
-  .add("with some emoji", () => (
-    <Button onClick={action("clicked")}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
+  .add("Input", () => <Input type="text" value="This is a v good input" />);
